75197688e415e41c1dd0942839471a8634dc7a90
Junpeng Qiu <qjpchmail@gmail.com>
2017-02-27 23:08:12 -0500

    Add Merriam-Webster

--- word-of-the-day.el
+++ word-of-the-day.el
@@ -28,7 +28,55 @@
 (require 'xml)
 (require 'shr)
 
+;; Steal from `elfeed'
+(defun wotd-xml-parse-region (&optional beg end buffer parse-dtd _parse-ns)
+  "Decode (if needed) and parse XML file. Uses coding system from
+XML encoding declaration."
+  (unless beg (setq beg (point-min)))
+  (unless end (setq end (point-max)))
+  (setf (point) beg)
+  (when (re-search-forward
+         "<\\?xml.*?encoding=[\"']\\([^\"']+\\)[\"'].*?\\?>" nil t)
+    (let ((coding-system (intern-soft (downcase (match-string 1)))))
+      (when (ignore-errors (check-coding-system coding-system))
+        (let ((mark-beg (make-marker))
+              (mark-end (make-marker)))
+          ;; Region changes with encoding, so use markers to track it.
+          (set-marker mark-beg beg)
+          (set-marker mark-end end)
+          (set-buffer-multibyte t)
+          (recode-region mark-beg mark-end coding-system 'raw-text)
+          (setf beg (marker-position mark-beg)
+                end (marker-position mark-end))))))
+  (let ((xml-default-ns ()))
+    (xml-parse-region beg end buffer parse-dtd 'symbol-qnames)))
 
+(defmacro wotd--retrieve (url sync &rest body)
+  (if sync
+      `(with-current-buffer (url-retrieve-synchronously ,url)
+         ,@body)
+    `(url-retrieve ,url
+                   (lambda (status)
+                     (if (plist-get status :error)
+                         (error "Error when retrieving %s" ,url)
+                       ,@body)))))
+
+(defun wotd--get-merriam-webster ()
+  (wotd--retrieve
+   "https://www.merriam-webster.com/wotd/feed/rss2" nil
+   (goto-char url-http-end-of-headers)
+   (let ((res-xml (wotd-xml-parse-region (point) (point-max)))
+         (res-buf (get-buffer-create "*Merriam-Webster*")))
+     (with-current-buffer res-buf
+       (erase-buffer)
+       (insert (nth 2 (car (xml-get-children
+                            (car (xml-get-children
+                                  (car (xml-get-children (car res-xml) 'channel))
+                                  'item))
+                            'description))))
+       (goto-char (point-min))
+       (replace-string "&#149;" "&#8226;")
+       (shr-render-buffer (current-buffer))))))
 
 (provide 'word-of-the-day)
 ;;; word-of-the-day.el ends here
