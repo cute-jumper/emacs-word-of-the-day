77d42d486ceaaa78fae7918b0553838109a23368
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-19 15:01:28 -0400

    Silence package linter

--- word-of-the-day.el
+++ word-of-the-day.el
@@ -4,6 +4,8 @@
 
 ;; Author: Junpeng Qiu <qjpchmail@gmail.com>
 ;; Keywords: extensions
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.4") (org "8.2.10"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
