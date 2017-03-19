763ce22d1944b723e3a90259e6ebe4770c38acb1
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-19 15:02:10 -0400

    Minor doc improvements

--- word-of-the-day.el
+++ word-of-the-day.el
@@ -22,7 +22,83 @@
 
 ;;; Commentary:
 
-;;
+;;                            _________________
+
+;;                             WORD OF THE DAY
+
+;;                               Junpeng Qiu
+;;                            _________________
+
+
+;; Table of Contents
+;; _________________
+
+;; 1 Available Sources
+;; 2 Commands
+;; .. 2.1 `M-x wotd-select'
+;; .. 2.2 `M-x wotd-all'
+;; 3 Config
+;; 4 Contribution
+
+
+;; Show /Word of the Day/ from 15 online sources in Emacs!
+
+
+;; 1 Available Sources
+;; ===================
+
+;;   - Merriam Webster
+;;   - Wiktionary
+;;   - Macmillan Dictionary
+;;   - Wordsmith
+;;   - Free Dictionary
+;;   - Oxford English Dictionary
+;;   - Urban Dictionary
+;;   - WordThink
+;;   - Oxford Dictionaries
+;;   - Cambridge Dictionary
+;;   - Collins Dictionary
+;;   - Learners Dictionary
+;;   - Wordnik
+;;   - Dictionary.com
+;;   - 5minuteenglish.com
+;;   - Bing dict (English word, Chinese definitions)
+
+
+;; 2 Commands
+;; ==========
+
+;; 2.1 `M-x wotd-select'
+;; ~~~~~~~~~~~~~~~~~~~~~
+
+;;   Show a word-of-the-day by selecting a source from
+;;   `wotd-enabled-sources'.
+
+;;   This works asynchronously.
+
+
+;; 2.2 `M-x wotd-all'
+;; ~~~~~~~~~~~~~~~~~~
+
+;;   Show all the "word-of-the-day"s from `wotd-enabled-sources' in a
+;;   summary buffer, which enables `orgstruct-mode' for easier navigation.
+
+;;   This works synchronously. (may be changed later)
+
+
+;; 3 Config
+;; ========
+
+;;   See `wotd-supported-sources' for the complete list of all the
+;;   supported online sources. You can set `wotd-enabled-sources' to
+;;   controls which sources are enabled. By default, all the sources except
+;;   `bing dict' are enabled.
+
+
+;; 4 Contribution
+;; ==============
+
+;;   Welcome to submit PRs to add more online sources!
 
 ;;; Code:
 
@@ -31,9 +107,6 @@
 (require 'shr)
 (require 'org-table)
 (require 'org)
-
-(defvar wotd-buffer "*Summary: Word of The Day")
-(defvar wotd-render-width (frame-width))
 
 (defvar wotd-supported-sources
   '(merriam-webster
@@ -51,9 +124,15 @@
     wordnik
     dictionary-dot-com
     5-minute-english
-    bing-dict))
+    bing-dict)
+  "The complete list of the supported sources.")
 
-(defvar wotd-enabled-sources (delq 'bing-dict wotd-supported-sources))
+(defvar wotd-enabled-sources (delq 'bing-dict wotd-supported-sources)
+  "This controls which sources should be enabled.
+See `wotd-supported-sources' for the complete list.")
+
+(defvar wotd-buffer "*Summary: Word of The Day")
+(defvar wotd-render-width (frame-width))
 
 (defvar wotd--enable-debug nil)
 (defvar wotd--debug-buffer "*WOTD Debug*")
@@ -67,7 +146,7 @@
   s)
 
 ;; Steal from `elfeed'
-(defun wotd-xml-parse-region (&optional beg end buffer parse-dtd _parse-ns)
+(defun wotd--xml-parse-region (&optional beg end buffer parse-dtd _parse-ns)
   "Decode (if needed) and parse XML file. Uses coding system from
 XML encoding declaration."
   (unless beg (setq beg (point-min)))
@@ -111,7 +190,7 @@ XML encoding declaration."
       (delete-region (point-min) (point))
       (let* (,(if (eq type 'html)
                   'it
-                '(it (wotd-xml-parse-region (point) (point-max))))
+                '(it (wotd--xml-parse-region (point) (point-max))))
              (res (progn ,@body))
              (content (if ,sync (cdr res) res))
              dom)
